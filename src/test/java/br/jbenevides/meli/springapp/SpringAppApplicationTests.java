664a3374c2deaa69e2718261a3743e7de59e40d3
jbenevides92 <jonatas.benevides@mercadolivre.com>
2022-11-01 16:00:37 -0300

    initial commit

---
+++ src/test/java/br/jbenevides/meli/springapp/SpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.jbenevides.meli.springapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
